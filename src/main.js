98294b610bd97832ac552bb1e5b6cc0e0adf6664
Matt DeVoe <matthew.devoe@gmail.com>
2019-03-16 15:08:07 -0600

    removed sample data

--- src/main.js
+++ src/main.js
@@ -2,9 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import Chart from 'vue2-frappe'
 
-Vue.use(VueAxios, axios, Chart)
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 

950a6dd248ea7eb8a8abf31f8ed952d6b5443290
Matt DeVoe <matthew.devoe@gmail.com>
2019-03-16 15:07:17 -0600

    removed vue2-frappe, drawing the owl didn't work

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -3,11 +3,45 @@
     <p> this is where the chart goes </p>
 
     <button @click="$emit('logged-out')">Log Out</button>
+    <template>
+        <vue-frappe
+            id="test"
+            :labels="[
+                '12am-3am', '3am-6am', '6am-9am', '9am-12pm',
+                '12pm-3pm', '3pm-6pm', '6pm-9pm', '9pm-12am'
+            ]"
+            title="My Awesome Chart"
+            type="axis-mixed"
+            :height="300"
+            :colors="['purple', '#ffa3ef', 'light-blue']"
+            :dataSets="this.data">
+        </vue-frappe>
+        </template>
   </div>
 </template>
 
 <script>
+import VueFrappe from 'vue2-frappe'
 export default {
+  data () {
+            return {
+                data: [{
+                    name: "Some Data", chartType: 'bar',
+                    values: [25, 40, 30, 35, 8, 52, 17, -4]
+                },
+                {
+                    name: "Another Set", chartType: 'bar',
+                    values: [25, 50, -10, 15, 18, 32, 27, 14]
+                },
+                {
+                    name: "Yet Another", chartType: 'line',
+                    values: [15, 20, -3, -15, 58, 12, -17, 37]
+                }]
+            }
+        },
+  components: {
+    VueFrappe
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import Chart from 'vue2-frappe'
 
-Vue.use(VueAxios, axios)
+Vue.use(VueAxios, axios, Chart)
 
 Vue.config.productionTip = false
 

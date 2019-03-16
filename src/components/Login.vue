32b2234f807d1f4430def7eba9e51d471a397de5
Matt DeVoe <matthew.devoe@gmail.com>
2019-03-16 15:44:30 -0600

    styled login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,31 +1,32 @@
 <template>
-  <div class="login">
-    <h5>welcome to trend tracker</h5>
-    <h5>please login to continue</h5>
-    <form>
-      <p>
-        <label>email: </label>
-        <input type="text"
-               autocomplete="email"
-               v-model="email">
-      </p>
-      <p>
-        <label>password: </label>
-        <input type="password"
-               autocomplete="current-password"
-               v-model="password">
-      </p>
-      <button @click.prevent="login">
-        Login
-      </button>
-    </form>
-    <trend
-      :data="[0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 12, 9, 11, 20]"
-      :gradient="['#6fa8dc', '#42b983', '#2c3e50']"
-      :auto-draw="true"
-      :autoDrawDuration="5000"
-      :smooth="false">
-    </trend>
+  <div>
+    <div class="login">
+      <form class="login-form">
+      <h3>welcome to trend tracker</h3>
+        <p>
+          <label>email: </label>
+          <input type="text"
+                autocomplete="email"
+                v-model="email">
+        </p>
+        <p>
+          <label>password: </label>
+          <input type="password"
+                autocomplete="current-password"
+                v-model="password">
+        </p>
+        <button @click.prevent="login">
+          Login
+        </button>
+      </form>
+    </div>
+      <trend
+        :data="[0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 12, 9, 11, 20]"
+        :gradient="['#6fa8dc', '#42b983', '#2c3e50']"
+        :auto-draw="true"
+        :autoDrawDuration="5000"
+        :smooth="false">
+      </trend>
   </div>
 </template>
 
@@ -61,4 +62,15 @@ export default {
 </script>
 
 <style scoped>
+.login {
+  position: relative;
+  top: 100px;
+}
+
+label {
+  display: inline-block;
+  width: 80px;
+  text-align: left;
+}
 </style>
+
